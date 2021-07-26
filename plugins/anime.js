2ba5e79a12f11baaf4122a3badc28b31ee7f10cf
D4rk-Dr4nzer <87435096+D4rk-Dr4nzer@users.noreply.github.com>
2021-07-26 23:39:43 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -94,7 +94,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by BABY BOT'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by 𝑩𝑨𝑩𝒀-𝑩𝑶𝑻🧚‍♂️'})
 
     }));
 }
@@ -188,7 +188,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'BABY BOT'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '𝑩𝑨𝑩𝒀-𝑩𝑶𝑻🧚‍♂️'})
 
     }));
 }
