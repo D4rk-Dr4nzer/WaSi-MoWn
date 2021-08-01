56a50df917ac4f35dab270895261126b335e9415
D4rk-Dr4nzer <87435096+D4rk-Dr4nzer@users.noreply.github.com>
2021-08-01 14:31:10 +0530

    Update lydia.js

--- plugins/sql/lydia.js
+++ plugins/sql/lydia.js
@@ -1,10 +1,8 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
-
+*/
 
 const config = require('../../config');
 const { DataTypes } = require('sequelize');
@@ -25,4 +23,3 @@ const LydiaDB = config.DATABASE.define('Lydia', {
 });
 
 module.exports = { LydiaDB: LydiaDB };
-*/
